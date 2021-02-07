d2fb1ee62631fc57a5ec0bb3904978f0c1aca811
Anton Pozdnyakov <Anton.Pozdnyakov.I@gmail.com>
2021-02-07 23:25:59 +0300

    Add app & postgresql settings

--- airtable4meta/airtable4meta/settings.py
+++ airtable4meta/airtable4meta/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tparser',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'airtable4meta.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_meta_db',
+        'USER' : 'duser',
+        'PASSWORD' : 'super',
+        'HOST' : '127.0.0.1',
+        'PORT' : '5432',
     }
 }
 
