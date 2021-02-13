efea7f6f51ae38daaef835c1ad1e361b460f870b
Anton Pozdnyakov <Anton.Pozdnyakov.I@gmail.com>
2021-02-14 00:03:13 +0300

    Add models to admin panel

--- airtable4meta/tparser/admin.py
+++ airtable4meta/tparser/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Rawdata, Method, Psychotherapist
 
-# Register your models here.
+@admin.register(Rawdata)
+class RawdataAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Method)
+class MethodAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Psychotherapist)
+class PsychotherapistAdmin(admin.ModelAdmin):
+    pass
