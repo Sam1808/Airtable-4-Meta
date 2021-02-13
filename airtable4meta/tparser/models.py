cafd889af23501a9436d4ecec8323e35cb97c2fd
Anton Pozdnyakov <Anton.Pozdnyakov.I@gmail.com>
2021-02-13 23:57:58 +0300

    Add models

--- airtable4meta/tparser/models.py
+++ airtable4meta/tparser/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Rawdata(models.Model):
+    upload_datetime = models.DateTimeField('Дата/время запуска', auto_now_add=True)
+    data = models.TextField('Данные',)
+
+
+class Method(models.Model):
+    method = models.CharField('Метод', max_length=30)
+
+    def __str__(self):
+        return self.method
+
+class Psychotherapist(models.Model):    
+    airtable_id = models.CharField('ID записи Airtable', max_length=18)
+    name = models.CharField('Имя', max_length=30)
+    image = models.ImageField("Фотография")
+    tag = models.ManyToManyField(Method, verbose_name='Методы', related_name='psychotherapists_tag')
+
+    def __str__(self):
+        return self.name
